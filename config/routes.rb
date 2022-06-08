c870d180bd671fb097980b946308ce745660c4d7
Jessie <jessie.baxter29@gmail.com>
2022-06-08 11:24:47 +1000

    Adding in project pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "project/maavens", to: "pages#maavens"
+  get "project/ui_kit", to: "pages#ui_kit"
 end
