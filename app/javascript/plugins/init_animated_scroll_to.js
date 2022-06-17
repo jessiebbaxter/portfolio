dcae273292a49af1d92a871d022e1d5ff8d1c4da
Jessie <jessie.baxter29@gmail.com>
2022-06-17 15:22:38 +1000

    Fixing bug where animate scroll was not being called on turbolink load event

--- app/javascript/plugins/init_animated_scroll_to.js
+++ app/javascript/plugins/init_animated_scroll_to.js
@@ -1,13 +1,17 @@
 import animateScrollTo from 'animated-scroll-to';
 
 const initAnimateScrollTo = () => {
-	const sidebar = document.getElementById("sidebar");
-	sidebar.addEventListener("click", (event) => {
-	  event.preventDefault();
-	  const href = event.target.hash
-	  const target = document.getElementById(href.replace('#',''));
-	  animateScrollTo(target, { verticalOffset: -10 } );
-	  window.history.pushState(null, null, href);
+	document.addEventListener('turbolinks:load', () => {
+		const sidebar = document.getElementById("sidebar");
+		if (sidebar != null) {
+			sidebar.addEventListener("click", (event) => {
+			  event.preventDefault();
+			  const href = event.target.hash
+			  const target = document.getElementById(href.replace('#',''));
+			  animateScrollTo(target, { verticalOffset: -10 } );
+			  window.history.pushState(null, null, href);
+			});
+		}
 	});
 };
 
