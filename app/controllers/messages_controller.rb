12f1828a00f324e8132d4828f97e6c7040987490
Jessie <jessie.baxter29@gmail.com>
2022-06-22 13:07:28 +1000

    Adding in rescue to catch Postmark API errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,17 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
       
     if @message.valid?
-      
-      MessageMailer.contact(@message).deliver_now
-      
-      redirect_to root_path
-      
-      flash[:notice] = "Thanks for reaching out, I'll get back to you shortly"
+      begin
+        MessageMailer.contact(@message).deliver_now
+
+        redirect_to root_path
+        
+        flash[:notice] = "Thanks for reaching out, I'll get back to you shortly"
+      rescue
+        redirect_to root_path
+
+        flash[:alert] = "That didn't quite work, please try again"
+      end
     else
       flash[:alert] = "That didn't quite work, please try again"
     end
