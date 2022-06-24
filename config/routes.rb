30fe5d5b9fd005aefe9e8906d38d2b5f0e04d5e4
Jessie <jessie.baxter29@gmail.com>
2022-06-24 11:43:55 +1000

    Removing postmark and letter_opener from app as unable to free SMTP gateway

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "project/maavens", to: "pages#maavens"
   get "project/ui_kit", to: "pages#ui_kit"
-  
-  resources :messages, only: [:create]
 end
